caf84745bb61dc4017c9d93ea07d5c988098502b
jsnam <jsnam3379@gmail.com>
2022-11-28 12:34:49 +0900

    added 2 handed feature for further function implementation

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from cv2 import cv2
 import time
 from cvzone.HandTrackingModule import HandDetector
 
-cap = cv2.VideoCapture(1)  # 0 for webcam, 1 for external
-detector = HandDetector(detectionCon=0.8, maxHands=1)
+cap = cv2.VideoCapture(0)  # 0 for webcam, 1 for external
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 pTime = 0
+tipIds = [4, 8, 12, 16, 20]
 
 while True:
     success, img = cap.read()
-    # img = cv2.flip(img, 1)  # flip for webcam
-    hands, img = detector.findHands(img)
+    img = cv2.flip(img, 1)  # flip for webcam
+    hands, img = detector.findHands(img, flipType=False)
 
     if hands:
         # hand1
@@ -19,8 +20,24 @@ while True:
         centerPoint1 = hand1["center"]  # center of the hand cx, cy
         handType1 = hand1["type"]  # hand type (left or right)
         fingers1 = detector.fingersUp(hand1)
+        for id in range(1, 5):
+            length, info, img = detector.findDistance(lmList1[4], lmList1[tipIds[id]], img)
 
         print(f"{handType1} Hand, Center = {centerPoint1}, Fingers = {fingers1}")
+
+        if len(hands) == 2:
+            # hand2
+            hand2 = hands[1]
+            lmList2 = hand2["lmList"]  # list of 21 landmarks of points
+            bbox2 = hand2["bbox"]  # bounding box info (x, y, w, h)
+            centerPoint2 = hand2["center"]  # center of the hand cx, cy
+            handType2 = hand2["type"]  # hand type (left or right)
+            fingers2 = detector.fingersUp(hand2)
+            for id in range(1, 5):
+                length, info, img = detector.findDistance(lmList2[4], lmList2[tipIds[id]], img)
+
+            print(f"{handType2} Hand, Center = {centerPoint2}, Fingers = {fingers2}")
+            print("")
 
     # show fps
     cTime = time.time()
